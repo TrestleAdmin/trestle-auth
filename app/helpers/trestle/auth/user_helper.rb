7353c3a96460d88277cd2c56213ce4bef3cdd4a8
Sam Pohlenz <sam@sampohlenz.com>
2017-05-24 16:01:13 +0930

    Support user models that don't respond to first_name/last_name

---
+++ app/helpers/trestle/auth/user_helper.rb
@@ -0,0 +1,9 @@
+module Trestle::Auth::UserHelper
+  def format_user_name(user)
+    if user.respond_to?(:first_name) && user.respond_to?(:last_name)
+      safe_join([user.first_name, content_tag(:strong, user.last_name)], " ")
+    else
+      display(user)
+    end
+  end
+end
--- lib/trestle/auth/engine.rb
+++ lib/trestle/auth/engine.rb
@@ -3,6 +3,10 @@ module Trestle
     class Engine < ::Rails::Engine
       config.assets.precompile << "trestle/auth.css" << "trestle/auth.js" << "trestle/auth/userbox.scss"
 
+      initializer "trestle.auth.helpers" do
+        Trestle::ApplicationController.helper Trestle::Auth::Engine.helpers
+      end
+
       config.to_prepare do
         Trestle::ApplicationController.send(:include, Trestle::Auth::ControllerMethods)
       end
