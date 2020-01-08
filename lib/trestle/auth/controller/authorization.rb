9f1a3458393418489683e78b6e3d2e7082bdba32
Sam Pohlenz <sam@sampohlenz.com>
2020-01-08 15:29:33 +1030

    Extract authorized? helper

--- lib/trestle/auth/controller/authorization.rb
+++ lib/trestle/auth/controller/authorization.rb
@@ -6,12 +6,17 @@ module Trestle
 
         included do
           before_action :authorize, if: :authorize?
+          helper_method :authorized?
           rescue_from AccessDenied, with: :unauthorized!
         end
 
       protected
+        def authorized?(action_name, target)
+          !authorize? || admin.authorization_adapter.authorized?(action_name.to_sym, target)
+        end
+
         def authorize
-          unless admin.authorization_adapter.authorized?(action_name.to_sym, authorization_target)
+          unless authorized?(action_name, authorization_target)
             raise AccessDenied, action: action_name.to_sym, target: authorization_target
           end
         end
@@ -20,21 +25,25 @@ module Trestle
           defined?(admin) && admin && admin.authorize?
         end
 
-        def authorization_target
-          nil
-        end
-
         def unauthorized!(exception)
           flash[:error] = {
             title:   t("admin.flash.unauthorized.title", default: "Access Denied!"),
             message: t("admin.flash.unauthorized.message", default: "You are not authorized to access this page.", message: exception.message)
           }
 
-          if admin.authorization_adapter.authorized?(:index, admin.model)
+          if authorized?(:index, root_authorization_target)
             redirect_to admin.path(:index)
           else
             redirect_to Trestle.config.root
           end
+        end
+
+        def authorization_target
+          nil
+        end
+
+        def root_authorization_target
+          nil
         end
       end
     end
--- lib/trestle/auth/resource.rb
+++ lib/trestle/auth/resource.rb
@@ -28,6 +28,10 @@ module Trestle
         def authorization_target
           instance || admin.model
         end
+
+        def root_authorization_target
+          admin.model
+        end
       end
     end
   end
