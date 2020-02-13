bc44ff2bf8a64ea7b5e378635daee126bf642f92
Sam Pohlenz <sam@sampohlenz.com>
2020-02-13 19:59:17 +1030

    Add Devise option (--devise) to install/admin generators

--- lib/generators/trestle/auth/admin/admin_generator.rb
+++ lib/generators/trestle/auth/admin/admin_generator.rb
@@ -6,10 +6,16 @@ module Trestle
 
         argument :model, type: :string, default: "Administrator"
 
+        class_option :devise, type: :boolean, default: false, desc: "Create admin for a Devise user model"
+
         source_root File.expand_path("../templates", __FILE__)
 
         def create_admin
           template "admin.rb.erb", File.join('app/admin/auth', "#{model.underscore.pluralize}_admin.rb")
+        end
+
+        def devise?
+          options[:devise]
         end
 
       protected
