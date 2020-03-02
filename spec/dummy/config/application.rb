eb973fbd86a56065f8b0d9cb2101873bfa009a18
Sam Pohlenz <sam@sampohlenz.com>
2020-03-03 09:32:39 +1030

    Load Rails config defaults in spec dummy app for version under test

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -13,12 +13,18 @@ require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
-require "trestle/auth"
 
 module Dummy
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    # Initialize configuration defaults for current Rails version.
+    case Rails.version.split(".").first(2).join(".")
+    when '6.0'
+      config.load_defaults 6.0
+    when '5.2'
+      config.load_defaults 5.2
+    when '5.1'
+      config.load_defaults 5.1
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
