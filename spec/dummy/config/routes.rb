9062254c7dbaf8de3dc474f1bff9e655bb85def9
Sam Pohlenz <sam@sampohlenz.com>
2021-05-10 14:26:56 +0930

    Update routing constraint (Trestle::Auth::Constraint) to work with all authentication backends

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   devise_for :devise_users, skip: :all
 end
+
+Trestle::Engine.routes.draw do
+  get "/custom", to: "custom#index", constraints: Trestle::Auth::Constraint.new
+end
